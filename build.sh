0f0f83cb7898359f89dd74289e0234b63a572c8c
Anthony Kioko <akioko367@gmail.com>
2025-05-29 10:08:46 +0300

    Update aptfile

--- build.sh
+++ build.sh
@@ -2,10 +2,6 @@
 # exit on error
 set -o errexit
 
-# Install system dependencies for document processing
-apt-get update
-apt-get install -y poppler-utils libmagic1
-
 # Install Python dependencies
 pip install -r requirements.txt
 
@@ -13,3 +9,4 @@ pip install -r requirements.txt
 mkdir -p uploads
 mkdir -p results
 mkdir -p static/annotated
+mkdir -p static/original
